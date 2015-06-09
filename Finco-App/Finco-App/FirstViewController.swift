d5f57dd90a25bf264ba2abd8876c802220b3ec78
Ævar Ísak Ástþórsson <aevarisak@gmail.com>
2015-06-09 22:21:08 +0000

    Initial app project

---
+++ Finco-App/Finco-App/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Finco-App
+//
+//  Created by Ævar Ástþórsson on 9.6.2015.
+//  Copyright (c) 2015 cakefactory. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
